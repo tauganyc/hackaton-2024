85e6f5702b2001a71539a1477c99a4905f17d047
taugany <tauganyc@hotmail.com>
2024-11-13 09:51:06 -0300

    Feat: dashboard e menu

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,9 +3,12 @@
 @section('content')
     <div class="row">
         <div class="col-md-12">
-            <h1 class="mt-4">Bem vindo!</h1>
-
-            TESTE
+            <h1 class="mt-4">Bem-vindo!</h1>
+            <p>A força de uma pequena ou média empresa não se mede apenas pelos lucros ou pela quantidade de produtos
+                que vende, mas pela coragem e resiliência de seus donos e colaboradores. E não há maior prova disso do
+                que a capacidade de recomeçar depois de um desastre inesperado, como uma enchente que destrói tudo o que
+                foi construído com tanto esforço e dedicação. Com isso, a Orange Investments traz uma forma lucrativa de
+                colocar sua empresa de volta no mercado!</p>
         </div>
     </div>
 @endsection
@@ -14,17 +17,18 @@
     setTimeout(function () {
         document.querySelector('.alert').remove();
     }, 3000);
+
     function copyToClipboard(id) {
-        const copyText = document.getElementById("copy_"+id);
+        const copyText = document.getElementById("copy_" + id);
         var textArea = document.createElement("textarea");
         textArea.value = copyText.value;
         document.body.appendChild(textArea);
         textArea.select();
         document.execCommand("Copy");
         textArea.remove();
-        document.getElementById("copy_"+id).innerHTML = "Copiado!";
+        document.getElementById("copy_" + id).innerHTML = "Copiado!";
         setTimeout(() => {
-            document.getElementById("copy_"+id).innerHTML = "Copiar";
+            document.getElementById("copy_" + id).innerHTML = "Copiar";
         }, 2000);
     }
 </script>
--- resources/views/layouts/navBarMenu.blade.php
+++ resources/views/layouts/navBarMenu.blade.php
@@ -2,12 +2,16 @@
     <div class="list-group list-group-flush" style="margin-top: 45px;">
 
         <a href="{{ route('extract') }}" class="list-group-item list-group-item-action">Extrato</a>
-        <a href="{{ route('company') }}"  class="list-group-item list-group-item-action">Proposta</a>
-        <form action="{{ route('logout') }}" method="post">
-            @csrf
-            <button type="submit" class="list-group-item list-group-item-action">Sair</button>
-        </form>
-{{--        <a class="list-group-item list-group-item-action mt-3">Sair</a>--}}
+
+        @if($user->type == 'empresa')
+            <a href="{{ route('company') }}" class="list-group-item list-group-item-action">Proposta</a>
+        @endif
+
+            <form action="{{ route('logout') }}" method="post">
+                @csrf
+                <button type="submit" class="list-group-item list-group-item-action">Sair</button>
+            </form>
+            {{--        <a class="list-group-item list-group-item-action mt-3">Sair</a>--}}
 
     </div>
 </div>
